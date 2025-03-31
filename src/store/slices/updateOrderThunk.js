7b763346b4a2fb59f85dbba9c406c00930e0c000
chavi-rotenberg <cr327744694@sgur.org.il>
2025-03-31 14:21:36 +0300

    update 12:58

--- src/store/slices/updateOrderThunk.js
+++ src/store/slices/updateOrderThunk.js
@@ -1,29 +1,4 @@
-<<<<<<< HEAD
-import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const updateOrderThunk = createAsyncThunk(
-    'updateOrderThunk',
-
-    async ({details}) => {
-
-        const response = await fetch("https://localhost:7044/api/Order/Update", {
-            method: 'PUT',
-            body: JSON.stringify(details),
-            headers: {
-                'Content-Type': "application/json"
-            }
-        });
-
-        if (response.ok) {
-            const data = await response.json();
-            return data
-        }
-        else {
-            throw new Error('failed to fetch update OrderThunk');
-        }
-    }
-
-=======
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 export const updateOrderThunk = createAsyncThunk(
@@ -48,5 +23,5 @@ export const updateOrderThunk = createAsyncThunk(
         }
     }
 
->>>>>>> ef9fa70721cd9013739de9d55184d1f9a325e648
+
 )
