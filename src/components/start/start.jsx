49eb4d918be0d082abf71afa41ccb6a379b9f96c
chavi-rotenberg <cr327744694@sgur.org.il>
2025-04-29 15:08:42 +0300

    finish comp startgit pushgit pushgit pushgit push�����

--- src/components/start/start.jsx
+++ src/components/start/start.jsx
@@ -1,24 +1,42 @@
 import { useNavigate } from "react-router-dom";
+import './start.css'
 import { Button } from "@mui/material";
 
-export const Start=()=>{
+export const Start = () => {
 
     const navigate = useNavigate();
 
-  return <div>
-  
-  
-  <div style={{width:"30%"}}>
-    <Button variant="contained" className='button' onClick={()=>navigate(`newOrder`)}>
-        לכניסה
-      </Button>
-      <Button variant="contained" className='button'   onClick={() => navigate(`myOrders`)}>
-       מנהל?
-      </Button>
-   </div>
-   <div style={{width:"30%"}} >
-   <img className='img' style={{width:"100%"}} src={process.env.PUBLIC_URL + "/start.jpg"} alt="" />
-   </div>
+    return <div style={{
+        display: "flex",
+        flexDirection: "row",
+        height:"100vh",
+        background:"#3b3a3d",
+        alignItems: "center"
+    }}>
 
-  </div>  
+
+        <div style={{
+            width: "30%",
+            margin: "5%",
+            display: "flex",
+            flexDirection: "column"
+        }}>
+            <Button variant="contained" style={{ width: "80%", marginTop: "5%" /*,boxShadow: "0 8px 20px #9b9595"border:"white solid 2px"*/}} className='strt_bt' onClick={() => navigate(`/home`)}>
+                לכניסה
+            </Button>
+            <br />
+            <Button variant="contained" style={{ width: "80%", marginTop: "5%"/*,boxShadow: "0 8px 20px #9b9595"border:"white solid 2px"*/ ,borderRadius:"15px "}
+        } className='strt_bt ' onClick={() => navigate(`/login`)}>
+                מנהל?
+            </Button>
+        </div>
+        <div style={{
+            width: "50%",
+            margin: "5%"
+
+        }} >
+            <img className='img' style={{ width: "100%", border: "none", borderRadius: "15px" ,boxShadow: "0 8px 20px #9b9595"}} src={process.env.PUBLIC_URL + "/start.jpg"} alt="" />
+        </div>
+
+    </div>
 }
